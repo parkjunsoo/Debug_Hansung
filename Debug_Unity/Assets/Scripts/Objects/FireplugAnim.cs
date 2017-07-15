eb4f3db761c7a46d9ed6d49cd3fbd6ad7f5595e7
jspark <junsoo76@daum.net>
2017-07-15 23:42:51 +0900

    170715Animation
    소화전 + 애니메이션 플레이를 위한 트리거 추가, 콜리더 및 붉은 라이트 추가(+스크립트까지)

---
+++ Debug_Unity/Assets/Scripts/Objects/FireplugAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireplugAnim : MonoBehaviour {
+
+    Animator anim;
+    bool animPlayed;
+
+	// Use this for initialization
+	void Awake() {
+        anim = GetComponent<Animator>();
+        animPlayed = false;
+    }
+
+    private void OnTriggerStay(Collider other)      //component 중에 isTrigger 체크된 Collider가 Trigger역할을 함
+    {                                               //Trigger 안에 다른 Collider가 들어와 있을 때 매 프레임마다 호출되는 함수
+        if(other.tag == "Player")                   //Trigger 안에 들어와 있는 오브젝트의 태그가 "Player"이면
+        {
+            if (animPlayed == false && Input.GetKeyDown(KeyCode.F))     //애니메이션을 한 번만 플레이하기 위해 animPlayed == false && F키를 눌렀을 때
+            {
+                anim.SetTrigger("Open");                //Animator에서 작용
+                animPlayed = true;                      //애니메이션을 한 번만 플레이해야 하므로 animPlayed를 true로 설정.
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+}
