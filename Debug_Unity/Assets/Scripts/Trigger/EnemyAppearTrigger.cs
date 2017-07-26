34c294d26dae0d033967664b6e240049c3262edf
jspark <junsoo76@daum.net>
2017-07-26 20:59:35 +0900

    170726_lightOff
    형광등 추가, LightOffTrigger 구현 >> TriggerEnter호출시 번개가 친 후 모든 형광등 라이팅 꺼짐

---
+++ Debug_Unity/Assets/Scripts/Trigger/EnemyAppearTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAppearTrigger : MonoBehaviour {
+
+    EnemyAppear parent;
+
+    // Use this for initialization
+    void Awake () {
+        parent = GetComponentInParent<EnemyAppear>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        parent.appear = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        parent.appear = false;
+    }
+}
---
+++ Debug_Unity/Assets/Scripts/Trigger/LightOffAfterThunder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOffAfterThunder : MonoBehaviour {
+
+    GameObject lampGroup;
+    Light[] lamp;
+    Light thunderLight;
+    bool isOff;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        isOff = false;
+        lampGroup = GameObject.Find("lampGroup");
+        lamp = lampGroup.GetComponentsInChildren<Light>();
+        thunderLight = GameObject.Find("Thunderlight").GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isOff)
+        {
+            isOff = true;
+            thunderLight.GetComponent<Thunder>().CallFast();
+            foreach (Light light in lamp)
+            {
+                light.enabled = false;
+            }
+        }
+    }
+
+}
