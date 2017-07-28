fb6ef449f3dddd79e69fc2d5dea3a3d6b9a7bb8d
kskk96 <kskk96@naver.com>
2017-07-28 20:59:56 +0900

    init

---
+++ DebugFPV/Assets/OutlineEffect/Demo/Toggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace cakeslice
+{
+    public class Toggle : MonoBehaviour
+    { 
+        // Use this for initialization
+        void Start()
+        {
+            GetComponent<Outline>().enabled = false;
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+                GetComponent<Outline>().enabled = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+                GetComponent<Outline>().enabled = false;
+        }
+    }
+}
---
+++ DebugFPV/Assets/Scripts/Objects/NipperExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NipperExample : MonoBehaviour {
+
+    GameObject playerCharacter;
+
+    public void Call()
+    {
+        var player = playerCharacter.GetComponent<PlayerItems>();         //player에, 가져온 playerCharacter의 PlayerControl 스크립트를 가져와 저장 
+        player.getNipper = true;                //Nipper를 얻었는지를 판단하는 부울 변수를 true로 설정
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Awake () {
+        playerCharacter = GameObject.Find("Player");                //이름이 "Player"인 GameObject를 가져와 playerCharacter 변수에 저장
+	}
+
+}
